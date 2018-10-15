9e15acc8a1827539cb4e20effd7fb780f7e928b6
Jonathan Collins <jrc.collins01@gmail.com>
2018-10-15 02:13:59 -0500

    Optimized web scraping with beautiful soup and Search.py with multithreading

--- menu_parser.py
+++ menu_parser.py
@@ -1,8 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import pprint as PP
 import datetime as Date
 import foodjson
+import re
 
 def get_url(date_arg,loc_arg):
 
@@ -40,67 +42,51 @@ def get_url(date_arg,loc_arg):
     url = 'http://hf-food.austin.utexas.edu/foodpro/menuSamp2.asp?' + vars
     return url
 
-class MyHTMLParser(HTMLParser):
 
-    def __init__(self):
-        super().__init__()
-        self.found_data = False
-        self.found_cat = False
-        self.found_meal = False
-        self.found_instruct=False
-        self.currmeal = ''
-        self.currcat = ''
-        self.map = {}
-        self.skip = False
+def read_html(date_arg,loc_arg):
+    meal_dict = {'Breakfast':{},'Lunch':{}, 'Dinner':{}}
 
-    def handle_starttag(self, tag, attrs):
-        #print("Start tag:", tag)
-        for attr in attrs:
-            if(attr[0] == 'class' and attr[1] == 'menusamprecipes'):
-                self.found_data = True
-            if(attr[0] == 'class' and attr[1] == 'menusampcats'):
-                self.found_cat = True
-            if(attr[0] == 'class' and attr[1] == 'menusampmeals'):
-                self.found_meal = True
-            if(attr[0] == 'class' and attr[1] == 'menusampinstructs'):
-                self.found_instruct = True
-
-
-    def handle_data(self, data):
-        if(self.skip==False):
-            if(self.found_data):
-                # only match specified meal
-                if(self.meal_time == self.currmeal):
-                    #print("Data     :", data)
-                    if(self.map.get(self.currcat) == None):
-                        self.map[self.currcat] = []
-                    self.map[self.currcat].append(data)
-                    self.found_data = False
-            if(self.found_cat):
-                self.found_cat = False
-                self.currcat = data
-            if(self.found_meal):
-                self.found_meal=False
-                self.currmeal = data
-            if(self.found_instruct):
-                self.found_instruct = False
-                if data.strip() == 'No Data Available':
-                    self.skip = True
-
-def read_html(date_arg,loc_arg,meal_time='Lunch'):
     url = get_url(date_arg,loc_arg)
     r = requests.get(url)
 
     #print(r.status_code)
     if(r.status_code == 200):
-        html = r.text
+        soup = BeautifulSoup(r.text, features="html.parser")
 
-    parser = MyHTMLParser()
-    parser.meal_time = meal_time
-    parser.feed(html)
 
-    foodjson.write_json(parser.map)
-    return parser.map
+        REGEXnl  = re.compile("\r|\n|\xa0")
+        REGEXsp = re.compile("(\\s\\s)+")
+        REGEXline = re.compile("(-- [A-Za-z0-9\\s&!@#$%^']+ --?)")
+        meals = soup.find("div", {"class" : "menusampmeals"})
+        if(meals == None):
+            return meal_dict
+        meals = meals.parent.parent.parent.parent.parent.parent.parent.parent
+        # print(meals)
+        meals = filter(lambda x : x is not None and x != "\n", meals.children)
+        meals = list(meals)
+        l = []
+
+
+        for meal in meals:
+            data = re.sub(REGEXnl," ", meal.text.strip())
+            data = re.sub(REGEXsp,"  ", data)
+            lines = re.split(REGEXline, data)
+            line_iter = iter(lines)
+            meal_time = next(line_iter).strip()
+            meal_dict[meal_time] = dict()
+
+            try:
+                while(True):
+                    line = next(line_iter)
+                    food = next(line_iter)
+                    meal_dict[meal_time][line] = []
+                    food = filter(lambda x: x.strip() !="", re.split(REGEXsp,food))
+                    food = list(map(lambda x : x.strip(), food))
+                    for food_item in food:
+                        meal_dict[meal_time][line].append(food_item)
+            except StopIteration as e:
+                pass
+    return meal_dict
 
 def command_line():
     J2= 'Jester+2nd+Floor+Dining'
@@ -123,21 +109,9 @@ def command_line():
     print()
     locIndex = input("Enter the number of the location: ")
     location = locations[int(locIndex)]
-    map = read_html('-'.join((year,month,day)),location,'Lunch')
-    print()
+    map = read_html('-'.join((year,month,day)),location)
 
-
-    i = 0
-    for line in map.keys():
-        print("{0}) {1}".format(i,line))
-        i=i+1
-    print()
-    lineIndex = input("Enter the number(s) of the line you want sep. by spaces: ")
-    lineIndex = lineIndex.split(" ")
-    print()
-    for num in lineIndex:
-        PP.pprint(map[list(map.keys())[int(num)]])
-        print("")
+    PP.pprint(map)
 
 
 if __name__ == '__main__':
--- search.py
+++ search.py
@@ -1,5 +1,8 @@
 from menu_parser import read_html
 from datetime import datetime, timedelta
+import pprint as PP
+import threading
+import time
 
 def find_day_for_food(food_item):
     # constants
@@ -24,54 +27,70 @@ def find_day_for_food(food_item):
     j2date_list = {'Lunch':[], 'Dinner':[]}
     kinsdate_list ={'Lunch':[], 'Dinner':[]}
     fastline_list = {'Lunch':[], 'Dinner':[]}
-    # for each date get the web data and if food item is contained in there, return date, location
-    for date in list:
-        dat = get_items_for_location_lunch(food_item, date, J2)
-        if len(dat) != 0:
-            j2date_list['Lunch'].append(dat)
-        dat = get_items_for_location_dinner(food_item, date, J2)
-        if len(dat) != 0:
-            j2date_list['Dinner'].append(dat)
-        dat = get_items_for_location_lunch(food_item, date, Kinsolving)
-        if len(dat) != 0:
-            kinsdate_list['Lunch'].append(dat)
-        dat = get_items_for_location_dinner(food_item,date, Kinsolving)
-        if len(dat) != 0:
-            kinsdate_list['Dinner'].append(dat)
-        dat = get_items_for_location_lunch(food_item,date, FastLine)
-        if len(dat) != 0:
-            fastline_list['Lunch'].append(dat)
-        dat = get_items_for_location_dinner(food_item,date, FastLine)
-        if len(dat) != 0:
-            fastline_list['Dinner'].append(dat)
 
+    # for each date get the web data and if food item is contained in there, return date, location
+    t_list = []
+    start = time.time()
+    # TODO: Make this faster
+    for date in list:
+        try:
+            thread_1 = threading.Thread(target=get_items_for_location_lunch, args=(food_item, date, J2, j2date_list['Lunch']))
+            thread_2 = threading.Thread(target=get_items_for_location_dinner, args=(food_item, date, J2, j2date_list['Dinner']))
+            thread_3 = threading.Thread(target=get_items_for_location_lunch, args=(food_item, date, Kinsolving, kinsdate_list['Lunch']))
+            thread_4 = threading.Thread(target=get_items_for_location_dinner, args=(food_item, date, Kinsolving, kinsdate_list['Dinner']))
+            thread_5 = threading.Thread(target=get_items_for_location_lunch, args=(food_item, date, FastLine, fastline_list['Lunch']))
+            thread_6 = threading.Thread(target=get_items_for_location_dinner, args=(food_item, date, FastLine, fastline_list['Dinner']))
+            t_list.append(thread_1)
+            t_list.append(thread_2)
+            t_list.append(thread_3)
+            t_list.append(thread_4)
+            t_list.append(thread_5)
+            t_list.append(thread_6)
+            thread_1.start()
+            thread_2.start()
+            thread_3.start()
+            thread_4.start()
+            thread_5.start()
+            thread_6.start()
+        except:
+            print("Error: unable to start thread")
+        # get_items_for_location_lunch(food_item, date, J2, j2date_list['Lunch'])
+        # get_items_for_location_dinner(food_item, date, J2, j2date_list['Dinner'])
+        # get_items_for_location_lunch(food_item, date, Kinsolving, kinsdate_list['Lunch'])
+        # get_items_for_location_dinner(food_item,date, Kinsolving, kinsdate_list['Dinner'])
+        # get_items_for_location_lunch(food_item,date, FastLine, fastline_list['Lunch'])
+        # get_items_for_location_dinner(food_item,date, FastLine, fastline_list['Dinner'])
+        for thread in t_list:
+            thread.join()
+    end = time.time()
+    print(end-start)
     ret_map = {}
     ret_map.update(J2=j2date_list)
     ret_map.update(Kinsolving=kinsdate_list)
     ret_map.update(FastLine=fastline_list)
     return ret_map
 
+# TODO: REMOVE THE NEED FOR THESE FUNCTIONS
+def get_items_for_location_lunch(food_item, date, loc, out_list):
+    return get_items_for_location(food_item,date,loc,'Lunch',out_list)
 
-def get_items_for_location_lunch(food_item, date, loc):
+def get_items_for_location_dinner(food_item, date, loc, out_list):
+    return get_items_for_location(food_item,date,loc,'Dinner',out_list)
+
+def get_items_for_location(food_item, date, loc, meal_time, out_list):
     date_map = {}
-    dictionary = read_html(date_arg=date,loc_arg=loc, meal_time='Lunch') #for lunch
+    ld_dictionary = read_html(date_arg=date,loc_arg=loc) #for lunch
+    # print(date, loc)
+    # PP.pprint(ld_dictionary)
+    dictionary = ld_dictionary[meal_time]
     for key in dictionary:
         if food_item in dictionary[key]:
             if(date_map.get(key) == None):
                 date_map[key] = []
             date_map[key].append(date)
             # print("Found it")
+    if(len(date_map) > 0):
+        out_list.append(date_map)
 
-    return date_map
-
-def get_items_for_location_dinner(food_item, date, loc):
-    date_map = {}
-    dictionary = read_html(date_arg=date,loc_arg=loc, meal_time='Dinner') #for lunch
-    for key in dictionary:
-        if food_item in dictionary[key]:
-            if(date_map.get(key) == None):
-                date_map[key] = []
-            date_map[key].append(date)
-            # print("Found it")
-
-    return date_map
+if __name__ == '__main__':
+    print(find_day_for_food("Chicken Noodle Soup"))
