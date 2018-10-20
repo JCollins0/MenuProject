8ae737a7f29249d95a8205cc5fa1afd1c8049199
Jonathan Collins <jrc.collins01@gmail.com>
2018-10-19 19:12:33 -0500

    Optimize search with half as many threads

--- search.py
+++ search.py
@@ -31,35 +31,19 @@ def find_day_for_food(food_item):
     # for each date get the web data and if food item is contained in there, return date, location
     t_list = []
     start = time.time()
-    # TODO: Make this faster
     for date in list:
         try:
-            thread_1 = threading.Thread(target=get_items_for_location_lunch, args=(food_item, date, J2, j2date_list['Lunch']))
-            thread_2 = threading.Thread(target=get_items_for_location_dinner, args=(food_item, date, J2, j2date_list['Dinner']))
-            thread_3 = threading.Thread(target=get_items_for_location_lunch, args=(food_item, date, Kinsolving, kinsdate_list['Lunch']))
-            thread_4 = threading.Thread(target=get_items_for_location_dinner, args=(food_item, date, Kinsolving, kinsdate_list['Dinner']))
-            thread_5 = threading.Thread(target=get_items_for_location_lunch, args=(food_item, date, FastLine, fastline_list['Lunch']))
-            thread_6 = threading.Thread(target=get_items_for_location_dinner, args=(food_item, date, FastLine, fastline_list['Dinner']))
+            thread_1 = threading.Thread(target=get_items_for_location, args=(food_item, date, J2, j2date_list))
+            thread_2 = threading.Thread(target=get_items_for_location, args=(food_item, date, Kinsolving, kinsdate_list))
+            thread_3 = threading.Thread(target=get_items_for_location, args=(food_item, date, FastLine, fastline_list))
             t_list.append(thread_1)
             t_list.append(thread_2)
             t_list.append(thread_3)
-            t_list.append(thread_4)
-            t_list.append(thread_5)
-            t_list.append(thread_6)
             thread_1.start()
             thread_2.start()
             thread_3.start()
-            thread_4.start()
-            thread_5.start()
-            thread_6.start()
         except:
             print("Error: unable to start thread")
-        # get_items_for_location_lunch(food_item, date, J2, j2date_list['Lunch'])
-        # get_items_for_location_dinner(food_item, date, J2, j2date_list['Dinner'])
-        # get_items_for_location_lunch(food_item, date, Kinsolving, kinsdate_list['Lunch'])
-        # get_items_for_location_dinner(food_item,date, Kinsolving, kinsdate_list['Dinner'])
-        # get_items_for_location_lunch(food_item,date, FastLine, fastline_list['Lunch'])
-        # get_items_for_location_dinner(food_item,date, FastLine, fastline_list['Dinner'])
         for thread in t_list:
             thread.join()
     end = time.time()
@@ -70,27 +54,22 @@ def find_day_for_food(food_item):
     ret_map.update(FastLine=fastline_list)
     return ret_map
 
-# TODO: REMOVE THE NEED FOR THESE FUNCTIONS
-def get_items_for_location_lunch(food_item, date, loc, out_list):
-    return get_items_for_location(food_item,date,loc,'Lunch',out_list)
 
-def get_items_for_location_dinner(food_item, date, loc, out_list):
-    return get_items_for_location(food_item,date,loc,'Dinner',out_list)
-
-def get_items_for_location(food_item, date, loc, meal_time, out_list):
+def get_items_for_location(food_item, date, loc, out_map):
     date_map = {}
-    ld_dictionary = read_html(date_arg=date,loc_arg=loc) #for lunch
+    ld_dictionary = read_html(date_arg=date,loc_arg=loc)
     # print(date, loc)
     # PP.pprint(ld_dictionary)
-    dictionary = ld_dictionary[meal_time]
-    for key in dictionary:
-        if food_item in dictionary[key]:
-            if(date_map.get(key) == None):
-                date_map[key] = []
-            date_map[key].append(date)
-            # print("Found it")
-    if(len(date_map) > 0):
-        out_list.append(date_map)
+    for meal_time in out_map:
+        dictionary = ld_dictionary[meal_time]
+        for key in dictionary:
+            if food_item in dictionary[key]:
+                if(date_map.get(key) == None):
+                    date_map[key] = []
+                date_map[key].append(date)
+                # print("Found it")
+        if(len(date_map) > 0):
+            out_map[meal_time].append(date_map)
 
 if __name__ == '__main__':
     print(find_day_for_food("Chicken Noodle Soup"))
